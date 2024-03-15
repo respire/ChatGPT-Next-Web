4bc54081abf6cfcbf8027efe6d5ad512d260dc0b
Respire <wingcat@live.com>
2024-03-15 05:58:41 +0000

    [devop]
    1. [bug] fix base url

--- build35.sh
+++ build35.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 DOCKER_BUILDKIT=1 docker build --build-arg OPENAI_API_KEY=$GPT35_KEY \
-             --build-arg BASE_URL="http://localhost:5555" \
+             --build-arg BASE_URL="http://127.0.0.1:5555" \
              --build-arg DISABLE_GPT4="1" \
              --build-arg CUSTOM_MODELS="-all,+gpt-3.5-turbo-0613,+gpt-3.5-turbo-1106,+gpt-3.5-turbo-16k,+gpt-3.5-turbo-16k-0613,+gpt-3.5-turbo,+gpt-3.5-turbo-0125,+gpt-3.5-turbo-0301,+gpt-3.5-turbo-instruct" \
              --no-cache \
