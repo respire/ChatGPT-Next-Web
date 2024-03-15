838b151fb902b428c664efd249a2f452adba2e17
Respire <wingcat@live.com>
2024-03-15 11:38:28 +0000

    [devop]
    1. [feature] add build4

---
+++ build4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+DOCKER_BUILDKIT=1 docker build --build-arg OPENAI_API_KEY=$GPT4_KEY \
+             --build-arg BASE_URL="http://dockerhost:5556" \
+             --build-arg DISABLE_GPT4="1" \
+             --build-arg CUSTOM_MODELS="-all,+gpt-4,+gpt-4-0125-preview,+gpt-4-0314,+gpt-4-0613,+gpt-4-1106-preview,+gpt-4-32k,+gpt-4-32k-0314,+gpt-4-32k-0613" \
+             --no-cache \
+             -f Dockerfile \
+             -t alicetyan-gpt4:latest \
+             .
