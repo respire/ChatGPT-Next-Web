50abdcea5eb5ae6d8a8adfa6052c799166de9487
Respire <wingcat@live.com>
2025-07-12 17:23:39 +0000

    remove plugin

--- app/store/plugin.ts
+++ app/store/plugin.ts
@@ -235,37 +235,6 @@ export const usePluginStore = createPersistStore(
       if (typeof window === "undefined") {
         return;
       }
-
-      fetch("./plugins.json")
-        .then((res) => res.json())
-        .then((res) => {
-          Promise.all(
-            res.map((item: any) =>
-              // skip get schema
-              state.get(item.id)
-                ? item
-                : fetch(item.schema)
-                    .then((res) => res.text())
-                    .then((content) => ({
-                      ...item,
-                      content,
-                    }))
-                    .catch((e) => item),
-            ),
-          ).then((builtinPlugins: any) => {
-            builtinPlugins
-              .filter((item: any) => item?.content)
-              .forEach((item: any) => {
-                const plugin = state.create(item);
-                state.updatePlugin(plugin.id, (plugin) => {
-                  const tool = FunctionToolService.add(plugin, true);
-                  plugin.title = tool.api.definition.info.title;
-                  plugin.version = tool.api.definition.info.version;
-                  plugin.builtin = true;
-                });
-              });
-          });
-        });
     },
   },
 );
